b8ac8b350d9b25340a82ae97ad3845f4e708af58
Vinícius Viçoso <vinicius_menezes@id.uff.br>
2017-10-03 15:52:31 -0300

    Imprimindo os valores y preditos

--- ajuste.c
+++ ajuste.c
@@ -6,7 +6,7 @@
 main()
 {
 	FILE *in;
-	double  X, Y, x = 0.0, y = 0.0, xx = 0.0, xy = 0.0, a0, a1;
+	double  X, Y, x = 0.0, y = 0.0, xx = 0.0, xy = 0.0, a0, a1, vx[t], vy[t];
 	int	i=0;
 	
 	
@@ -21,6 +21,8 @@ main()
 		
 		xx+=pow(X,2);
 		
+		vx[i] = X;
+		vy[i] = Y;
 		
 		i++;
 	}
@@ -29,6 +31,22 @@ main()
 	a1=((t*xy) - (x*y)) / ((t*xx) - (pow(x,2)));
 	a0=((xx*y) - (xy*x)) / ((t*xx) - (pow(x,2)));
 	
+	
 	printf("\nA1 = %lf\nA0 = %lf\n",a1,a0);
+	
+	
+	fclose(in);
+	
+	
+	in=fopen("ajdados.dat", "w");
+	
+	
+	for(i=0; i < t; i++)
+
+		fprintf(in,"%lf\t%lf\t%lf\n",vx[i],vy[i],((a0 * vx[i]) + a1));
+		
+	
+	fclose(in);
+	
 }
 	
